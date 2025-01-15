568baa1408c8a975660cdfe64d177121bb04e192
nicof-fulmar <156094170+nicof-fulmar@users.noreply.github.com>
2025-01-15 22:58:29 +0200

    Update

--- fulmar/tango/layer1/src/main/java/com/fulmar/layer1/service/TangoL1IncomingMessageProcessorService.kt
+++ fulmar/tango/layer1/src/main/java/com/fulmar/layer1/service/TangoL1IncomingMessageProcessorService.kt
@@ -1,9 +1,12 @@
 package com.fulmar.layer1.service
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
 
 suspend fun tangoL1IncomingMessageProcessorService(
     messages: Flow<ByteArray>,
 ) {
+    messages.collect { tMessage->
 
+    }
 }
